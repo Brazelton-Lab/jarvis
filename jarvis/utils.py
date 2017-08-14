627ee3761a3326aa1d2a31a7d50e23d8029f805a
TheOneHyer <theonehyer@gmail.com>
2017-08-13 18:31:11 -0600

    Added autocomplete to utils
    Added an autocomplete function to utils and added error message
    if no match found.

--- jarvis/utils.py
+++ jarvis/utils.py
@@ -5,20 +5,20 @@ Contains various functions for use by jarvis.py
 """
 
 import argparse
+from difflib import get_close_matches
 
 __author__ = 'Alex Hyer, Christopher Thornton'
 __email__ = 'theonehyer@gmail.com'
 __license__ = 'GPLv3'
 __maintainer__ = 'Alex Hyer'
 __status__ = 'Alpha'
-__version__ = '2.0.0a2'
+__version__ = '2.0.0a3'
 
 
 class ParseCommas(argparse.Action):
     """Argparse Action that parses arguments by commas
 
     Attributes:
-
         option_strings (list): list of str giving command line flags that
                                call this action
 
@@ -50,7 +50,6 @@ class ParseCommas(argparse.Action):
         Simply split list by commas and add to namespace.
 
         Args:
-
             parser (ArgumentParser): parser used to generate values
 
             namespace (Namespace): namespace to set values for
@@ -60,7 +59,6 @@ class ParseCommas(argparse.Action):
             option_string (str): argument flag used to call this function
 
         Raises:
-
             TypeError: if values is not a string
 
             ValueError: if value cannot, for any reason, be parsed
@@ -82,11 +80,56 @@ class ParseCommas(argparse.Action):
         setattr(namespace, self.dest, arguments)
 
 
+def autocorrect(query, possibilities, delta=0.75):
+    """Attempts to figure out what possibility the query is
+
+    Args:
+        query (unicode): query to attempt to complete
+
+        possibilities (list): list of unicodes of possible answers for query
+
+        delta (float): minimum delta similarity between query and
+                       any given possibility for possibility to be considered.
+                       Delta used by difflib.get_close_matches().
+
+    Returns:
+        unicode: best guess of correct answer
+
+    Raises:
+        AssertionError: raised if no matches found
+
+    Example:
+    >>> autocomplete('bowtei', ['bowtie2', 'bot']
+    'bowtie2'
+    """
+
+    possibilities = [possibility.lower() for possibility in possibilities]
+
+    # Don't waste time for exact matches
+    if query in possibilities:
+        return query
+
+    # Complete query as much as possible
+    options = [word for word in possibilities if word.startswith(query)]
+    if len(options) > 0:
+        possibilities = options  # possibilities now limited to options
+        query = max_substring(options)
+
+    # Identify possible matches and return best match
+    matches = get_close_matches(query, possibilities, cutoff=delta)
+
+    try:
+        assert len(matches) > 0
+    except AssertionError:
+        raise AssertionError('No matches for "{0}" found'.format(query))
+
+    return matches[0]
+
+
 def max_substring(words, last_letter='', position=0):
     """Finds max substring shared by all strings starting at position
 
     Args:
-
         words (list): list of unicode of all words to compare
 
         last_letter (unicode): letter of last common letter, only for use
